297b2b106756e65899e2d0634bf355efd51afd26
connorhoward <conhow@med.umich.edu>
2024-07-02 17:49:14 -0400

    Optimized settings for Desired Report

--- gpt_researcher/config/config.py
+++ gpt_researcher/config/config.py
@@ -17,10 +17,10 @@ class Config:
         self.fast_llm_model = os.getenv('FAST_LLM_MODEL', "gpt-4-turbo")
         self.smart_llm_model = os.getenv('SMART_LLM_MODEL', "gpt-4o")
         self.fast_token_limit = int(os.getenv('FAST_TOKEN_LIMIT', 4000))
-        self.smart_token_limit = int(os.getenv('SMART_TOKEN_LIMIT', 8000))
+        self.smart_token_limit = int(os.getenv('SMART_TOKEN_LIMIT', 4000))
         self.browse_chunk_max_length = int(os.getenv('BROWSE_CHUNK_MAX_LENGTH', 8192))
         self.summary_token_limit = int(os.getenv('SUMMARY_TOKEN_LIMIT', 700))
-        self.temperature = float(os.getenv('TEMPERATURE', 0.55))
+        self.temperature = float(os.getenv('TEMPERATURE', 0.1))
         self.user_agent = os.getenv('USER_AGENT', "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                                                    "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0")
         self.max_search_results_per_query = int(os.getenv('MAX_SEARCH_RESULTS_PER_QUERY', 7))
